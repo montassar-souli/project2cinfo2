3a7b65e3561d3cabc728afb11be08d8588162c9b
Montassar Souli <montassar.souli@esprit.tn>
2024-10-01 20:23:55 +0100

    Update routing configuration and add not-found component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  //activated = new ActivatedRoute(); ==> probléme Couplage fort
 
+  listProducts: Product[] = [
+    {
+      id: 1,
+      name: 'iphone',
+      price: 1000,
+      image: 'assets/images/phone.jpeg',
+      categoryId: 1,
+    },
+    {
+      id: 2,
+      name: 'tv',
+      price: 3000,
+      image: 'assets/images/phone.jpeg',
+      categoryId: 2,
+    },
+    {
+      id: 3,
+      name: 'iphone',
+      price: 1000,
+      image: 'assets/images/phone.jpeg',
+      categoryId: 1,
+    },
+  ];
+
+  // Injection de dépendance (patron de conception)
+  constructor(private activated: ActivatedRoute) {
+    console.log(this.activated.snapshot.params);
+  }
 }
