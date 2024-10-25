717ddc22f8b84f555528d10885794a7d7cb1cef4
Montassar Souli <montassar.souli@esprit.tn>
2024-10-25 19:56:53 +0100

    Refactor add-product component to improve form layout, validation, and API integration

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -7,13 +9,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent {
+  constructor(private ps: ProductService, private r: Router) {}
   product: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required,Validators.pattern("[A-Z][A-Za-z0-9_éàè]*")]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[A-Z][A-Za-z0-9_éàè]*'),
+    ]),
     price: new FormControl('', Validators.min(0)),
     image: new FormControl('', [Validators.required]),
   });
   submit() {
-    console.log(this.product.value);
+    this.ps.addProduct(this.product.value);
+    this.r.navigate(['/home']);
   }
   //
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http:HttpClient) { }
+
+  getProducts(){
+    return this.http.get('http://localhost:3000/products');
+  }
+  addProduct(product:Product){
+    return this.http.post(`http://localhost:3000/products/${product.id}`,product);
+  }
+  updateProduct(product:Product){
+    return this.http.put(`http://localhost:3000/products/${product.id}`,product);
+  }
+  deleteProduct(id:number){
+    return this.http.delete(`http://localhost:3000/products/${id}`);
+  }
+  getProductById(id:number){
+    return this.http.get(`http://localhost:3000/products/${id}`);
+  }
+}
